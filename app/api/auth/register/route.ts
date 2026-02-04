19004f2c7dc1b0131259a24e2b1bcaae0b030c3b
LeksG <grand781278@gmail.com>
2026-02-04 21:04:18 +0200

    Fix Decimal type errr

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,6 +2,8 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs'; // <--- Імпорт
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
